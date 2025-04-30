6154081417c7f483129bdbeb1b2f07b6c6f389cc
Lily <lilymillard343@gmail.com>
2025-04-29 22:48:01 -0400

    fixes for server

--- server.js
+++ server.js
@@ -2,13 +2,30 @@
 
 const path = require('path');
 require("dotenv").config({ path: path.join(process.env.HOME, '.cs304env')});
+const express = require('express');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+
 const { Connection } = require('./connection');
 const cs304 = require('./cs304');
-const mongoUri = cs304.getMongoUri();
 
-const express = require('express')
-const app = express()
-const port = 8080
+// Create and configure the app
+
+const app = express();
+
+app.use(cs304.logStartRequest);
+
+// This handles POST data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cs304.logRequestData);  // tell the user about any request data
+
+app.use(serveStatic('public'));
+app.set('view engine', 'ejs');
+
+const mongoUri = cs304.getMongoUri();
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
